b4a2fc3a2b39a7242f27ff4c76050f0f0ada85b7
Mone19 <fuocoangelos@yahoo.de>
2024-05-29 20:27:21 +0200

    create Theme Switcher

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen dark:bg-black">
-      <h1 className="text-3xl mb-4 text-white">Home</h1>
-      <p className="text-lg mb-4 text-white">Das ist die Startseite</p>
+    <div className="mb-4 flex flex-col justify-center items-center min-h-[calc(100vh-200px)] dark:bg-black">
+      <h1 className="text-2xl mb-2 text-white">Home</h1>
+      <p className="text-md mb-2 text-white">Das ist die Startseite</p>
       <img className="flex-col" src="https://www.limesoda.com/blog/wp-content/uploads/aufbau-blogseite-header-770x404.png" alt="" />
     </div>
   );
